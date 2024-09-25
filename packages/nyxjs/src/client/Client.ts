4b460f6934416a06ff17726500ba7d10abf1a9a7
3tatsu <minecraftytom22@gmail.com>
2024-09-25 19:53:02 +0200

    refactor(nyxjs): remove utils.ts and update Client.ts
    - Delete packages/nyxjs/src/helpers/utils.ts
    - Update packages/nyxjs/src/client/Client.ts

--- packages/nyxjs/src/client/Client.ts
+++ packages/nyxjs/src/client/Client.ts
@@ -1,10 +1,13 @@
 import type { ApiVersions, GatewayIntents, Integer } from "@nyxjs/core";
+import { BitfieldManager } from "@nyxjs/core";
 import type { RestOptions } from "@nyxjs/rest";
 import { Rest } from "@nyxjs/rest";
 import type { GatewayOptions } from "@nyxjs/ws";
 import { Gateway } from "@nyxjs/ws";
 import { EventEmitter } from "eventemitter3";
-import { restOptions, wsOptions } from "../helpers/utils";
+import { DEFAULT_API_VERSION, DEFAULT_ENCODING } from "../helpers/constants";
+
+const options = Symbol("options");
 
 export type ClientOptions = {
     intents: GatewayIntents[] | Integer;
@@ -26,20 +29,26 @@ export class Client extends EventEmitter<ClientEvents> {
 
     public rest: Rest;
 
-    public token!: string;
+    public token: string | null;
 
-    public constructor(options: ClientOptions) {
+    private [options]: ClientOptions;
+
+    public constructor(initialOptions: ClientOptions) {
         super();
-        this.ws = new Gateway(this.token, wsOptions(options));
-        this.rest = new Rest(this.token, restOptions(options));
+        this.token = null;
+        this[options] = initialOptions;
+        this.ws = new Gateway(this.token!, this.createWsOptions());
+        this.rest = new Rest(this.token!, this.createRestOptions());
+        this.setupEventListeners();
     }
 
     public async connect(token: string): Promise<void> {
+        if (!token || typeof token !== "string") {
+            throw new Error("Invalid token provided");
+        }
+
         try {
             this.token = token;
-            this.ws.on("error", (error) => this.emit("error", error));
-            this.ws.on("warn", (message) => this.emit("warn", message));
-            this.ws.on("debug", (message) => this.emit("debug", message));
             await this.ws.connect();
         } catch (error) {
             await this.destroy();
@@ -55,5 +64,41 @@ export class Client extends EventEmitter<ClientEvents> {
         this.ws.disconnect();
         await this.rest.destroy();
         this.removeAllListeners();
+        this.token = null;
+    }
+
+    private calculateIntents(intents: GatewayIntents[] | Integer): number {
+        if (Array.isArray(intents)) {
+            return Number(BitfieldManager.from(intents).valueOf());
+        } else {
+            return intents;
+        }
+    }
+
+    private createWsOptions(): GatewayOptions {
+        return {
+            presence: this[options].presence,
+            shard: this[options].shard,
+            v: this[options].version ?? DEFAULT_API_VERSION,
+            intents: this.calculateIntents(this[options].intents),
+            encoding: this[options].ws?.encoding ?? DEFAULT_ENCODING,
+            compress: this[options].ws?.compress,
+            large_threshold: this[options].ws?.large_threshold,
+        };
+    }
+
+    private createRestOptions(): RestOptions {
+        return {
+            auth_type: this[options].rest?.auth_type,
+            cache_life_time: this[options].rest?.cache_life_time,
+            user_agent: this[options].rest?.user_agent,
+            version: this[options].version ?? DEFAULT_API_VERSION,
+        };
+    }
+
+    private setupEventListeners(): void {
+        this.ws.on("error", (error) => this.emit("error", error));
+        this.ws.on("warn", (message) => this.emit("warn", message));
+        this.ws.on("debug", (message) => this.emit("debug", message));
     }
 }
