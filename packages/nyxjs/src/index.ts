23211be59f19030101fed7e66fd4a68f7beb1ae2
AtsuLeVrai <minecraftytom22@gmail.com>
2025-04-03 22:15:58 +0200

    refactor: re-export all packages in index.ts for improved module accessibility

--- packages/nyxjs/src/index.ts
+++ packages/nyxjs/src/index.ts
@@ -5,6 +5,7 @@ export * from "./data/index.js";
 export * from "./options/index.js";
 export * from "./types/index.js";
 
+// Re-export all the packages
 export * from "@nyxjs/builders";
 export * from "@nyxjs/core";
 export * from "@nyxjs/gateway";
